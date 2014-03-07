c10450701bcdac9adebe4e324182b1de2bd970cc
Ed Shadi <edshadi@gmail.com>
2014-03-06 21:05:26 -0800

    linked list node

---
+++ test/data-structures/linked-list/nodeSpec.js
@@ -0,0 +1,23 @@
+require('../../test_helper')
+describe('Node', function () {
+  var data = "sweet data",
+      node;
+  beforeEach(function () {
+    node = new Node(data);
+  });
+  describe('#data', function () {
+    it('is initialized to the passed in value', function () {
+      expect(node.data).to.eq(data);
+    });
+    it('we are able to set it', function () {
+      var anoterData = "another data";
+      node.data = anoterData;
+      expect(node.data).to.eq(anoterData);
+    });
+  });
+  describe('#next', function () {
+    it('is initialized to nil', function () {
+      expect(node.next).be.an('undefined');
+    });
+  });
+});
