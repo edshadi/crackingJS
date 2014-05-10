84e2a4a35ffa1168fbd3a0bff2bba337133c0796
Ricardo Rubio <rickarubio@gmail.com>
2014-05-10 01:13:10 -0700

    finished adding failing tests for isPermutation()

--- test/strings/isPermutationSpec.js
+++ test/strings/isPermutationSpec.js
@@ -7,4 +7,22 @@ describe('isPermutation', function() {
     var str2 = "lhba";
     expect(isPermutation(str1, str2)).to.equal(true);
   });
+
+  it('returns true if str2 is a permutation of str1', function() {
+    var str1 = "the quick brown fox 12345"
+    var str2 = "quick   thebrownfox 21543"
+    expect(isPermutation(str1, str2)).to.equal(true);
+  });
+
+  it('returns false if str2 is not a permutation of str1', function() {
+    var str1 = "blah";
+    var str2 = "bleh";
+    expect(isPermutation(str1, str2)).to.equal(false);
+  });
+
+  it('returns false if str2 is not a permutation of str1', function() {
+    var str1 = "apples";
+    var str2 = "appples";
+    expect(isPermutation(str1, str2)).to.equal(false);
+  })
 });
